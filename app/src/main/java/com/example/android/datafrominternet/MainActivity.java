5726812614509a91ca4e709486264ddc5b632f0e
Divya Gupta <divagupta2392@gmail.com>
2017-04-18 12:26:20 +0530

    Asynctask added

--- app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.datafrominternet;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -26,6 +27,7 @@ import android.widget.Toast;
 
 import com.example.android.datafrominternet.utilities.NetworkUtils;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView mUrlDisplayTextView;
 
-    TextView mSearchResult;
+    TextView mSearchResultTextView;
 
 
     @Override
@@ -44,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         mSearchBoxEditText = (EditText)findViewById(R.id.et_search_box);
         mUrlDisplayTextView = (TextView)findViewById(R.id.tv_url_display);
-        mSearchResult = (TextView)findViewById(R.id.tv_github_search_results_json);
+        mSearchResultTextView = (TextView)findViewById(R.id.tv_github_search_results_json);
 
     }
     /**
@@ -58,6 +60,34 @@ public class MainActivity extends AppCompatActivity {
         String githubQuery = mSearchBoxEditText.getText().toString();
         URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
         mUrlDisplayTextView.setText(githubSearchUrl.toString());
+
+        String githubSearchResults = null;
+        new GithubQueryTask().execute(githubSearchUrl);
+    }
+
+    public class GithubQueryTask extends AsyncTask<URL, Void, String>
+    {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String githubSearchResults = null;
+            try {
+                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return githubSearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(s != null && !s.equals(""))
+            {
+                mSearchResultTextView.setText(s);
+            }
+        }
     }
 
     @Override
